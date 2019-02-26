5e5796a71d043bf9890221627c3bfe278110ba2b
kyruski <jamescboyett@gmail.com>
2019-02-26 17:29:53 -0600

    Adds outline of tests

---
+++ test/server-tests.js
@@ -0,0 +1,17 @@
+const assert = require('assert');
+const expect = require('chai').expect;
+const should = require('chai').should();
+
+describe('server communication', () => {
+  
+  it('should return true if valid user id', function(){
+        // const isValid = loginController.isValidUserId('abc123')
+        // //assert.equal(isValid, true);
+        // expect(isValid).to.be.true;
+  });
+  it('should return false if invalid user id', function(){
+        // const isValid = loginController.isValidUserId('abc1234')
+        // //assert.equal(isValid, false);
+        // isValid.should.equal(false);
+  });
+})
