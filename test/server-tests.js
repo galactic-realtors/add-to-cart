03816bce8815c54d2df9314716261dc68fee5d04
kyruski <jamescboyett@gmail.com>
2019-02-27 14:40:03 -0600

    Removes unneeded section in server

--- test/server-tests.js
+++ test/server-tests.js
@@ -3,15 +3,14 @@ const expect = require('chai').expect;
 const should = require('chai').should();
 
 describe('server communication', () => {
-  
   it('should return true if valid user id', function(){
-        // const isValid = loginController.isValidUserId('abc123')
-        // //assert.equal(isValid, true);
-        // expect(isValid).to.be.true;
+		// const isValid = loginController.isValidUserId('abc123')
+		// //assert.equal(isValid, true);
+		// expect(isValid).to.be.true;
   });
   it('should return false if invalid user id', function(){
-        // const isValid = loginController.isValidUserId('abc1234')
-        // //assert.equal(isValid, false);
-        // isValid.should.equal(false);
+		// const isValid = loginController.isValidUserId('abc1234')
+		// //assert.equal(isValid, false);
+		// isValid.should.equal(false);
   });
 })
