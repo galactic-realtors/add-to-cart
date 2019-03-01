b28610888441e4eb36b62529ca5c8ae56078587c
kyruski <jamescboyett@gmail.com>
2019-03-01 16:14:22 -0600

    Testing env variables w/ travis pt2

--- test/db-tests.js
+++ test/db-tests.js
@@ -1,7 +1,7 @@
 const assert = require('assert');
 const expect = require('chai').expect;
 const db = require('../database/index');
-//test
+
 (function() {
   'use strict';
   describe('database communication', () => {
