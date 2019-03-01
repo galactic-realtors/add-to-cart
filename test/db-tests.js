fbccdb68b6a6cae9a39a12e9d33e282775f2cf19
kyruski <jamescboyett@gmail.com>
2019-03-01 17:03:05 -0600

    Again?!?!@?#?!?R#@$

--- test/db-tests.js
+++ test/db-tests.js
@@ -2,7 +2,7 @@ const assert = require('assert');
 const expect = require('chai').expect;
 const db = require('../database/index');
 
-(function() {
+(function() { //once again
   'use strict';
   describe('database communication', () => {
     let id = 69;
