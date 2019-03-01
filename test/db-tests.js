6cae9077406f3a69f0d764410be7e0f340f5bebb
kyruski <jamescboyett@gmail.com>
2019-03-01 16:33:06 -0600

    Again

--- test/db-tests.js
+++ test/db-tests.js
@@ -7,7 +7,6 @@ const db = require('../database/index');
 (function() {
   'use strict';
   describe('database communication', () => {
-    console.log('dis be process env', process.env);
     let id = 69;
     it('should return an object', function(done){
       
@@ -20,6 +19,7 @@ const db = require('../database/index');
         done()
       })
       .catch((err) => {
+        console.log('this be err', err);
         expect(false).to.equal(true);
         done()
       });
