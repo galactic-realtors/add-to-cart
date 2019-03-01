83a26d3b61eede6187f8b710d318bdb3edc60f15
kyruski <jamescboyett@gmail.com>
2019-03-01 14:48:09 -0600

    Updates travis.yml file

--- test/db-tests.js
+++ test/db-tests.js
@@ -1,6 +1,6 @@
 const assert = require('assert');
 const expect = require('chai').expect;
-const { getAllProducts } = require('../database/index');
+const db = require('../database/index');
 
 (function() {
   'use strict';
@@ -8,26 +8,48 @@ const { getAllProducts } = require('../database/index');
     
     let id = 69;
     it('should return an object', function(done){
-      getAllProducts(id, (err, data) => {
-        const isObj = (typeof data === 'object' && Array.isArray(data) === false);
-        expect(isObj).to.be.true;
-        done();
+      
+      db('testinsert')
+      .select()
+      .where('id', `${id}`)
+      .then((data) => {
+        const isObj = (typeof data[0] === 'object' && Array.isArray(data[0]) === false);
+        expect(isObj).to.equal(true);
+        done()
       })
+      .catch((err) => {
+        expect(false).to.equal(true);
+        done()
+      });
     });
     it('should return the the correct desired object', function(done){
-      getAllProducts(id, (err, data) => {
-        expect(data.id).to.equal(69);
-        expect(data.product_name).to.equal('Chicken - Whole');
-        expect(data.price).to.equal('$5352.52');
-        done();
+      db('testinsert')
+      .select()
+      .where('id', `${id}`)
+      .then((data) => {
+        expect(data[0].id).to.equal(69);
+        expect(data[0].product_name).to.equal('Chicken - Whole');
+        expect(data[0].price).to.equal('$5352.52');
+        done()
       })
+      .catch((err) => {
+        expect(false).to.equal(true);
+        done()
+      });
     });
 
     it('should return an error when an invalid ID is supplied', function(done){
-      getAllProducts('abc', (err, data) => {
-        expect(err).to.be.an('error');
-        done();
+      db('testinsert')
+      .select()
+      .where('id', `${id}`)
+      .then((data) => {
+        expect(false).to.equal(true);
+        done()
       })
+      .catch((err) => {
+        expect(true).to.equal(true);
+        done()
+      });
     });
 
   })
