f5c042bdac8d56f69ff1fe6833ee2c3c9a6e3885
kyruski <jamescboyett@gmail.com>
2019-03-02 10:52:35 -0600

    Updates some of the seeding scripts and alters the front end to reflect the correct price generated

--- database/seedingfilecreatorcsv.js
+++ database/seedingfilecreatorcsv.js
@@ -1,12 +1,13 @@
 const fs = require('fs');
-const cart = require('./cart');
+const faker = require('faker');
 
-fs.appendFileSync('./seedingfile.csv', 'id|product_name|price\n');
+fs.appendFileSync('./seedingfile.csv', 'product_name|price\n');
 
-for (let i = 0; i < 100000; i++) {
-  for (let j = 0; j < cart.length; j++) {
-    let item = cart[j];
-    let id = item.id + (i * 100);
-    fs.appendFileSync('./seedingfile.csv', `${id}|${item.product_name}|${item.price}\n`);
+for (let i = 0; i < 10000000; i++) {
+  let product_name = faker.commerce.productName();
+  let price = faker.commerce.price();
+  fs.appendFileSync('./seedingfile.csv', `${i + 1}|${product_name}|${price}\n`);
+  if (i % 100000 === 0) {
+    console.log(i / 100000);
   }
 }
